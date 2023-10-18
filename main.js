86b50706937c85dc3a89c3b2b38e2e0f9f8551b2
sami903 <146079758+sami903@users.noreply.github.com>
2023-10-18 17:07:52 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function add(){
+    
+    var name1=document.getElementById("input1").value;
+    document.getElementById("label1").textContent=name1;
+    
+    var name2=document.getElementById("input2").value;
+    document.getElementById("label2").textContent=name2;
+    
+    var name3=document.getElementById("input3").value;
+    document.getElementById("label3").textContent=name3;
+    
+    
+    
+}
